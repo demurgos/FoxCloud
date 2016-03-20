10534744da91c58d0a2903b749fe38072f7f333a
Mathieu Razafimahazo <mathieu.razafimahazo@gmail.com>
2016-03-20 19:11:58 +0100

    Header and content box splitted

--- app/app.js
+++ app/app.js
@@ -12,50 +12,50 @@
 	'nvd3',
 	'datatables',
 	'adminLTE']);
-    
+
     // Configure routes
     app.config(['$routeProvider',
-		function($routeProvider) {
-		    $routeProvider.
-			when('/dashboard', {
-			    templateUrl: 'build/html/DashboardView.html',
-			    controller: 'DashboardController',
-			    pageName: 'Counters / Sites Overview',
-			    category: 'Counters'
-			}).
-			when('/current_user', {
-			    templateUrl: 'build/html/CurrentUserView.html',
-			    pageName : "My account",
-			    controller: 'CurrentUser',
-			    category : "Settings"
-			}).
-			when('/settings_site_items', {
-			    templateUrl: 'build/html/SettingsSiteItemsView.html',
-			    controller: 'SettingsSiteItems',
-			    pageName : "Site cameras",
-			    category : "Settings"
-			}).
-			when('/settings_users', {
-			    templateUrl: 'build/html/SettingsUsersView.html',
-			    controller: 'SettingsUsers',
-			    pageName : "User management",
-			    category : "Settings"
-			}).
-			when('/settings_sites', {
-			    templateUrl: 'build/html/SettingsSitesView.html',
-			    controller: 'SettingsSites',
-			    pageName : "Site management",
-			    category : "Settings"
-			}).
-			when('/settings_site_users', {
-			    templateUrl: 'build/html/SettingsSiteUsersView.html',
-			    controller: 'SettingsSiteUsers',
-			    pageName : "Site users",
-			    category : "Settings"
-			}).
-			otherwise({
-			    redirectTo: '/dashboard'
-			});
-		}
-	       ]);
+        function($routeProvider) {
+            $routeProvider.
+                when('/dashboard', {
+                    templateUrl: 'build/html/DashboardView.html',
+                    controller: 'DashboardController',
+                    pageName: 'Counters / Sites Overview',
+                    category: 'Counters'
+                }).
+                when('/current_user', {
+                    templateUrl: 'build/html/CurrentUserView.html',
+                    pageName : "My account",
+                    controller: 'CurrentUser',
+                    category : "Settings"
+                }).
+                when('/settings_site_items', {
+                    templateUrl: 'build/html/SettingsSiteItemsView.html',
+                    controller: 'SettingsSiteItems',
+                    pageName : "Site cameras",
+                    category : "Settings"
+                }).
+                when('/settings_users', {
+                    templateUrl: 'build/html/SettingsUsersView.html',
+                    controller: 'SettingsUsers',
+                    pageName : "User management",
+                    category : "Settings"
+                }).
+                when('/settings_sites', {
+                    templateUrl: 'build/html/SettingsSitesView.html',
+                    controller: 'SettingsSites',
+                    pageName : "Site management",
+                    category : "Settings"
+                }).
+                when('/settings_site_users', {
+                    templateUrl: 'build/html/SettingsSiteUsersView.html',
+                    controller: 'SettingsSiteUsers',
+                    pageName : "Site users",
+                    category : "Settings"
+                }).
+                otherwise({
+                    redirectTo: '/dashboard'
+                });
+        }
+    ]);
 }());
